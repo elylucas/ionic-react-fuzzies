2cbf7840767ab7f10db77f5c47489193149d6229
Ely Lucas <ely@meta-tek.net>
2020-05-20 10:11:20 -0600

    updates to starting point

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
 
 const Home: React.FC = () => {
   return (
@@ -17,7 +15,6 @@ const Home: React.FC = () => {
             <IonTitle size="large">Kids</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
